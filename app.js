55ef3ecfb34b442efe084e2be31d35a683212bbb
= <=>
2012-12-01 18:43:15 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+	routes = require('./routes'),
+	http = require('http'),
+	path = require('path');
+
+var app = express();
+
+app.configure(function(){
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler());
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log("Express server listening on port " + app.get('port'));
+});
+
+// Routes
+
+app.get('/', function(req, res){
+	res.render('index', {
+		title: 'picpost'
+	});
+});
+
+app.get('/about', function(req, res){
+	res.render('about', {
+		title: 'About'
+	});
+});
